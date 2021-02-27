1c3a5f10ab7abc3569515e642b71d07c17c68ddd
edarsh7 <edarsh7@gmail.com>
2021-02-26 17:53:30 -0800

    idk

--- fileman.c
+++ fileman.c
@@ -54,11 +54,11 @@ int fileman_write(char *fname, size_t foffset, char *buf, size_t boffset, size_t
 {
 	//use creat()
 	//rm -rf data.dir before running fileman b
-	int file = creat(fname, S_IWUSR);
-	if(file == -1)
+	int file = 0;
+	file = creat(fname, S_IWUSR);
+	if(file == 0)
 		return -1;
-	else
-		printf("wtf man\n");
+	
 
 }
 
