890620a7ebac942f31c59c92465fd80a7dc4d786
edarsh7 <edarsh7@gmail.com>
2021-02-27 02:56:34 -0800

    idk

--- fileman.c
+++ fileman.c
@@ -142,9 +142,10 @@ void fileman_dir(int fd, char *dname)
 		if(strcmp(dp->d_name, ".") == 0   || strcmp(dp->d_name, "..") == 0)
 			continue;
 		temp = dp->d_name;
+		printf("%s \n",dp->d_name);
 		if((dir = opendir(temp)) == NULL)
 			break;
-		printf("%s \n",dp->d_name);
+		
 	}
 
 	
