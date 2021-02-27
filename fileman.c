8d43bd7562615eb94e6ce6b5c64dea8909550aae
edarsh7 <edarsh7@gmail.com>
2021-02-26 22:16:10 -0800

    idk

--- fileman.c
+++ fileman.c
@@ -56,7 +56,7 @@ int fileman_write(char *fname, size_t foffset, char *buf, size_t boffset, size_t
 {
 
 	int file = creat(fname, 0777);
-	if(file == -1)
+	if(errno == EEXIST)
 		return -1;
 		
 	for(;boffset > 0 ; boffset--)
