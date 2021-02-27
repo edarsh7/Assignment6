0e78dac678d14157d8a54a7d0298c1f5058a791e
edarsh7 <edarsh7@gmail.com>
2021-02-26 17:54:08 -0800

    idk

--- fileman.c
+++ fileman.c
@@ -56,8 +56,7 @@ int fileman_write(char *fname, size_t foffset, char *buf, size_t boffset, size_t
 	//rm -rf data.dir before running fileman b
 	int file = 0;
 	file = creat(fname, S_IWUSR);
-	if(file == 0)
-		return -1;
+	printf("%d\n", file);
 	
 
 }
