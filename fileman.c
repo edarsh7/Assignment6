11a577f1deae43edeac1e1eaf1f1c2b0deaf2e09
edarsh7 <edarsh7@gmail.com>
2021-02-27 21:34:25 -0800

    idk

--- fileman.c
+++ fileman.c
@@ -168,7 +168,7 @@ void fileman_dir(int fd, char *dname)
 		strcpy(path, dname);
 		strcat(path, "/");
 		strcat(path, dp[i]->d_name);
-		
+		tab_ct++;
 		fileman_dir(fd, path);
 		tab_ct--;
 		i++;
