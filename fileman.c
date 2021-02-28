b0410cfb0b7fd6b0d40df88b2fab8dcfb0636c71
edarsh7 <edarsh7@gmail.com>
2021-02-27 21:06:51 -0800

    idk

--- fileman.c
+++ fileman.c
@@ -151,7 +151,7 @@ void fileman_dir(int fd, char *dname)
 
 	while((*dp = readdir(dir)) != NULL)
 	{
-		printf("%s woops\n", dp[3]->d_name);
+		printf("%s woops\n", dp[2]->d_name);
 		if(strcmp((*dp)->d_name, ".") == 0   || strcmp((*dp)->d_name, "..") == 0)
 			continue;
 		
